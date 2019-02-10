a94d99871a50c2abbca7acbb87bb335e0bb98072
Henry Salazar Serrano <henrysteven003@gmail.com>
2019-02-10 14:16:02 -0800

    project working on aws and node_modules to upgrade in lambda function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { WebhookClient, Suggestion } = require('dialogflow-fulfillment');
+const {WebhookClient, Suggestion} = require('dialogflow-fulfillment');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const compression = require('compression');
@@ -9,7 +9,6 @@ const awsServerlessExpressMiddleware = require('aws-serverless-express/middlewar
 var express = require('express');
 const app = express();
 const router = express.Router();
-const unirest = require("unirest");
 
 router.use(compression());
 router.use(cors());
@@ -17,30 +16,19 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(awsServerlessExpressMiddleware.eventContext());
 
-router.post('/webhooks/dialogflow', (request, response) => {
+router.post('/', (request, response) => {
     const agent = new WebhookClient({ request, response });
-    //console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-    //console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-    var body = request.body;
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
 
-    function welcome(agent) {
-        agent.add(`Welcome to my agent on AWS Lambda!`);
-    }
-
-    function fallback(agent) {
-        agent.add(`I didn't understand`);
-        agent.add(`I'm sorry, can you try again?`);
-    }
-
-    function horarios(agent) {
-        let biblioteca = agent.parameters.biblioteca;
-        agent.add("Hola, soy fullfilment y voy a buscar el horario de la bilbioteca " + biblioteca);
-    }
+  function horarios(agent) {
+    var biblioteca = agent.parameters.biblioteca;
+    agent.add("Hola, soy bibliobot desde aws y voy a buscar el horario de la bilbioteca " + biblioteca);
+  }
 
     // Run the proper function handler based on the matched Dialogflow intent name
     let intentMap = new Map();
-    intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('Default Fallback Intent', fallback);
     intentMap.set("Horarios bibliotecas", horarios);
     agent.handleRequest(intentMap);
 });
@@ -48,13 +36,3 @@ router.post('/webhooks/dialogflow', (request, response) => {
 app.use('/', router);
 
 module.exports = app;
-
-
-
-/*
-var
-    port = process.env.PORT || 3000;
-app.listen(port);
-
-console.log('Server started on: ' + port);
-*/
