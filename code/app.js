b2620196b20a10cad08fbbecebe0d9f9877dc4e9
Henry Salazar Serrano <henrysteven003@gmail.com>
2019-02-16 22:32:21 -0800

    searching books done - all logic is ok - cards for showing a good ui...henrysalazarv0.1

--- code/app.js
+++ code/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { WebhookClient, Suggestion } = require('dialogflow-fulfillment');
+const { WebhookClient, Suggestion, Card } = require('dialogflow-fulfillment');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const compression = require('compression');
@@ -9,6 +9,7 @@ const awsServerlessExpressMiddleware = require('aws-serverless-express/middlewar
 var express = require('express');
 const app = express();
 const router = express.Router();
+const bibloredImgUrl = 'http://www.bogota.gov.co/sites/default/files/styles/large/public/field/image/logo-biblored-actualizado.jpg?itok=aiOgTXBa';
 
 router.use(compression());
 router.use(cors());
@@ -24,13 +25,40 @@ router.post('/', (request, response) => {
 
   function schedules(agent) {
     var biblioteca = agent.parameters.biblioteca;
-    agent.add("Hola Mundo19, soy bibliobot y voy a buscar el horario de la bilbioteca " + biblioteca);
+    agent.add("Hola, soy bibliobot y voy a buscar el horario de la bilbioteca " + biblioteca);
   }
 
   function bookSearch(agent) {
-    var book = agent.query;
-    agent.add(book);
-    //agent.add((book + "").includes('el libro'));
+    var query = agent.query;
+    /* agent.add("el libro es " + query);
+    agent.add('EL LIBRO ES: ' + JSON.stringify(query)); */
+    var strQuery = JSON.stringify(query);
+    var queryType = -1;
+    if (strQuery.includes('libro')) {
+      queryType = 1;
+    } else if (strQuery.includes('autor')) {
+      queryType = 2;
+    } else if (strQuery.includes('genero') || strQuery.includes('género') || strQuery.includes('tipo')) {
+      queryType = 3;
+    }
+    if (1 === queryType) {
+      var splitter = JSON.stringify(query).split("libro")[1].split(",");
+      //agent.add("Hola bibliobot va a buscar el libro " + splitter[0] + "");
+
+      agent.add('Bibliobot soy yo,');
+      agent.add(new Card({
+        title: 'Voy a buscar el libro en segundos vuelvo...',
+        imageUrl: bibloredImgUrl,
+        text: splitter[0] + ' 💁',
+        buttonText: 'Echa un vistazo a biblored',
+        buttonUrl: 'http://catalogo.biblored.gov.co/'
+      })
+      )
+    } else {
+      agent.add('Especifica que quieres buscar por ejemplo "Buscar el libro <libro>"')
+    }
+
+    //agent.add(JSON.stringify(book).includes('el libro'));
     /* if (book.includes('el libro')) {
       var splitter = JSON.stringify(book).split(" ");
       for (var i = 0; i < splitter.length; ++i) {
---
+++ code/zipGenerate.sh
@@ -0,0 +1,4 @@
+clear
+rm -rf code.zip
+zip -r codelinux.zip node_modules app.js index.js package.json package-lock.json
+clear
