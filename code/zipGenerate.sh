6b97dfdaf2e9471fa039802043e6a1edd5074632
Henry Salazar <henrysteven003@gmail.com>
2019-04-28 01:11:03 -0500

    deleted folders

--- code/zipGenerate.sh
+++
@@ -1,9 +0,0 @@
-clear
-rm -rf codelinux.zip
-zip -r codelinux.zip node_modules app.js index.js package.json package-lock.json
-rm -rf /home/bibliobot/Documents/ToM/s3Files/codelinux.zip
-mv codelinux.zip /home/bibliobot/Documents/ToM/s3Files/
-cd /home/bibliobot/Documents/ToM/s3Files
-/home/bibliobot/bin/aws s3 sync . s3://bibliobot-bucket/
-/home/bibliobot/bin/aws lambda update-function-code --function-name bibliobot --s3-bucket bibliobot-bucket --s3-key codelinux.zip
-#clear
